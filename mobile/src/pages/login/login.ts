b10e119a4a5dbaaadd05ad7171decc71545f25d1
Gustavo Silva <silva95gustavo@gmail.com>
2018-04-13 23:36:08 +0100

    ...

--- mobile/src/pages/login/login.ts
+++ mobile/src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
+import { Platform } from 'ionic-angular';
 
 /**
  * Generated class for the LoginPage page.
@@ -14,12 +16,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  mobileDevice: boolean
+  constructor(public navCtrl: NavController, public navParams: NavParams, private backgroundGeolocation: BackgroundGeolocation,
+    public platform: Platform) {
+      if (this.platform.is('core')) {
+        this.mobileDevice = false
+      } else {
+        this.mobileDevice = true
+      }
+      if (this.mobileDevice) {
+        const config: BackgroundGeolocationConfig = {
+          desiredAccuracy: 10,
+          stationaryRadius: 20,
+          distanceFilter: 30,
+          debug: false,
+          startOnBoot: true,
+          stopOnTerminate: false,
+        };
+        this.backgroundGeolocation.configure(config).subscribe((location: BackgroundGeolocationResponse) => {
+          console.log(location)
+        });
+      }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+    this.startPlaying()
+  }
+
+  startPlaying() : void {
+    console.log('aaaaaaaa')
+    if (!this.mobileDevice) {
+      console.warn('Cannot start background geolocation because the app is not being run in a mobile device.')
+      return
+    }
+    this.backgroundGeolocation.start()
+  }
+
+  stopPlaying() : void {
+    if (this.mobileDevice) {
+      this.backgroundGeolocation.stop()
+    }
   }
 
 }
