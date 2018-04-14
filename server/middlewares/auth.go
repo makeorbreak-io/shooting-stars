0a16f6fca4a1db208dba65ed73ab00ac05dcb73d
João Silva <hello@joaosilva.co>
2018-04-14 14:04:05 +0100

    Finished authentication middleware.

--- server/middlewares/auth.go
+++ server/middlewares/auth.go
@@ -2,21 +2,29 @@ package middlewares
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/makeorbreak-io/shooting-stars/server/core"
+	"github.com/makeorbreak-io/shooting-stars/server/models"
 	"net/http"
 )
 
 // HandleAuthentication validates the authorization token
-func HandleAuthentication() gin.HandlerFunc {
+func HandleAuthentication(authService models.IAuthService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("Authorization")
 		if token == "" {
-			c.AbortWithStatus(http.StatusUnauthorized)
+			c.AbortWithError(http.StatusUnauthorized, core.ErrorNotLogged).
+				SetType(gin.ErrorTypePublic)
 			return
 		}
 
-		userID := 1 // TODO: Get user ID from authentication service
-		c.Set("userID", userID)
+		userID, err := authService.GetUserIDByToken(token)
+		if err != nil {
+			c.AbortWithError(http.StatusUnauthorized, core.ErrorNotLogged).
+				SetType(gin.ErrorTypePublic)
+			return
+		}
 
+		c.Set("userID", userID)
 		c.Next()
 	}
 }
