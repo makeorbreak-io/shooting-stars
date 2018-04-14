c279d0b268d2e88c26b6c5ff92ec96139e76cb3d
guilhermevpinto <guilherme.pinto@xpand-it.com>
2018-04-14 13:12:36 +0100

    correction

--- mobile/src/pages/login/login.ts
+++ mobile/src/pages/login/login.ts
@@ -3,7 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
 import { Platform } from 'ionic-angular';
 import { GamePageModule } from '../game/game.module';
-import { GamePage } from '../game/game';
 import { RegisterPage } from '../register/register';
 import { ApiProvider } from '../../providers/api/api';
 import { AuthProvider } from '../../providers/auth/auth';
