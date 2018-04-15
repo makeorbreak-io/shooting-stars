801e9a11dc4d2a193fc1083d6042ae5ffe5caaf8
Gustavo Silva <silva95gustavo@gmail.com>
2018-04-15 14:10:38 +0100

    ...

--- mobile/src/pages/login/login.module.ts
+++ mobile/src/pages/login/login.module.ts
@@ -3,12 +3,7 @@ import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [],
-=======
-  declarations: [
-  ],
->>>>>>> 5ae21c5081217d6acf3843d0f93764af8c4c3e08
   imports: [
     IonicPageModule.forChild(LoginPage),
   ],
